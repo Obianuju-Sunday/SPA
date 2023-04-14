b99202538bd140a44481d15de185525aef1e36ab
Obianuju-Sunday <obianujusunday43@gmail.com>
2023-04-14 21:13:07 +0100

    spa commit

---
+++ Js/Barber.js
@@ -0,0 +1,6 @@
+var name = prompt("Firstname");
+var name = prompt("Surname");
+var name = prompt("Age");
+var name = prompt("Mental Status  (Normal,Phycho)");
+var name = prompt("Marital Status  (Single,Searching,Divorced,Married)");
+alert("Thank you!");
